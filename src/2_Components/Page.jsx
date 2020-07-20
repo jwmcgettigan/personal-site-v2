e226509a979160dac2606d8988a1a0277ebfc8fa
Justin McGettigan <jwmcgettigan@gmail.com>
2020-07-20 11:57:20 -0500

    Reimplemented overflow: hidden

--- src/2_Components/Page.jsx
+++ src/2_Components/Page.jsx
@@ -61,7 +61,7 @@ const Page = ({ page, renderedPage, className }) => {
   const containerStyle = css(`
     display: grid;
     grid-template-rows: min-content auto;
-    //overflow: hidden;
+    overflow: hidden;
 
     ${mq('tablet-wide')} {
       grid-template-columns: min-content auto;
